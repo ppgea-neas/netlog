ee1326d6344b0c83e89926c65f88f67969060520
Jabes <jabesr@gmail.com>
2019-05-15 15:43:11 -0300

    Updates

---
+++ precache-manifest.83d14904aa8e12eda88d33b10d18fc21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfe4f572795987e95060bd1612eccfac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc13651ecaaf2eb5f9c0",
+    "url": "./static/css/main.6ad062ef.chunk.css"
+  },
+  {
+    "revision": "0924e4346a59ff8882f8",
+    "url": "./static/js/2.0800d27f.chunk.js"
+  },
+  {
+    "revision": "bc13651ecaaf2eb5f9c0",
+    "url": "./static/js/main.466f32c1.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  }
+]);
